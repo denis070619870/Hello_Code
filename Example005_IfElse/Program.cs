d9af914a5305f15ac766891dfbfad2adb4a8e502
Denis <denis070619870@rambler.ru>
2022-08-04 13:26:04 +0300

    Example005_IfElse

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите ваше имя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "Денис")
+{
+    Console.WriteLine("Добро пожаловать, Денис!");
+}
+else
+{
+    Console.Write("Привет!");
+    Console.WriteLine(username);
+}
