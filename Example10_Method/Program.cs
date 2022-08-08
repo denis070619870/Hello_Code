e21b0ae3d004c68c030f5fe0eb6ae6b7bd85cf2b
Denis <denis070619870@rambler.ru>
2022-08-08 15:33:51 +0300

    Example10_Method

---
+++ Example10_Method/Program.cs
@@ -0,0 +1,19 @@
+﻿// Найти элемент в массиве от 0 до n, равный find
+
+int[] array = {34,66,66,33,67,9,5,90,18};
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    //index =index + 1;
+    index++;
+}
